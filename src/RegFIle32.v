26650fa2ab13eeb1bfb2266c7db346e1be7a61b3
x402 <akr01@qq.com>
2022-05-11 11:21:31 +0800

    rewrite button clock

--- src/RegFIle32.v
+++ src/RegFIle32.v
@@ -2,7 +2,7 @@
 
 module RegFile32(
     input clk,
-    input rst_n,
+    input rst,
     input [4:0] rs1,
     input [4:0] rs2,
     input [4:0] rd,
@@ -30,7 +30,7 @@ module RegFile32(
     genvar j;
     generate
         for(j = 1; j < 32; j = j + 1) begin: reg_gen
-            Reg #(32, 0) u_reg(clk, rst_n, wen[j], data_in, q[j]);
+            Reg #(32, 0) u_reg(clk, rst, wen[j], data_in, q[j]);
         end
     endgenerate
 endmodule
